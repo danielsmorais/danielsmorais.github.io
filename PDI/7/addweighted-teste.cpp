f73eaf9c967e35cc7b7bb565e5436cf6d4e0836e
danielsmorais <danielmorais@outlook.com.br>
2017-04-02 00:25:00 -0300

    PDI

--- PDI/7/addweighted-teste.cpp
+++ PDI/7/addweighted-teste.cpp
@@ -14,6 +14,12 @@ int top_slider_max = 100;
 Mat image1, image2, blended;
 Mat imageTop; 
 
+Mat frame32f, frameFiltered;
+
+float media[] = { 1, 1, 1,
+                  1, 1, 1,
+                  1, 1, 1
+};
 char TrackbarName[50];
 
 void on_trackbar_blend(int, void*){
@@ -24,7 +30,7 @@ void on_trackbar_blend(int, void*){
 
 void on_trackbar_line(int, void*){
   image1.copyTo(imageTop);
-  int limit = top_slider*255/100;
+  int limit = top_slider*628/100;
   if(limit > 0){
 	Mat tmp = image2(Rect(0, 0, 256, limit));
 	tmp.copyTo(imageTop(Rect(0, 0, 256, limit)));
@@ -33,9 +39,14 @@ void on_trackbar_line(int, void*){
 }
 
 int main(int argvc, char** argv){
-  image1 = imread("blend1.jpg");
-  image2 = imread("blend2.jpg");
+  image1 = imread("../imagens/woman.jpg");          //628x418
+  image2 = imread("../imagens/woman.jpg");
+  for(int i=0; i<10; i++)  
+    GaussianBlur(image2, image2, Size(9,9),0,0);
+      
   image2.copyTo(imageTop);
+  
+  
   namedWindow("addweighted", 1);
   
   sprintf( TrackbarName, "Alpha x %d", alfa_slider_max );
@@ -46,6 +57,13 @@ int main(int argvc, char** argv){
   on_trackbar_blend(alfa_slider, 0 );
   
   sprintf( TrackbarName, "Scanline x %d", top_slider_max );
+  createTrackbar( TrackbarName, "addweighted",
+				  &top_slider,
+				  top_slider_max,
+				  on_trackbar_line );
+  on_trackbar_line(top_slider, 0 );
+  
+  sprintf( TrackbarName, "Decaimento x %d", top_slider_max );
   createTrackbar( TrackbarName, "addweighted",
 				  &top_slider,
 				  top_slider_max,
